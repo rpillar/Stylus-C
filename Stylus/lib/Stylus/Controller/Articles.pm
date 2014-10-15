3cfa69d1f8f528b71354f0e9429505ad134588c9
Richard Pillar <richardpillar@googlemail.com>
2014-10-15 09:12:58 +0100

    updates ...

--- Stylus/lib/Stylus/Controller/Articles.pm
+++ Stylus/lib/Stylus/Controller/Articles.pm
@@ -51,7 +51,6 @@ sub auto :Private {
 =cut
 
 sub index :Path( '/stylus/articles' ) :Args(0) {
-    #my ( $self, $c, $domain ) = @_;
     my ( $self, $c ) = @_;
 
     # set initial content for 'landing' page
@@ -61,8 +60,8 @@ sub index :Path( '/stylus/articles' ) :Args(0) {
 	$c->stash->{righthalf} = 'articlesright.tt';
 	
 	# get articles data - set initial article values
-	#my @articles = $c->model('DB::Article')->find({ domain => $domain });
-	my @articles = $c->model('DB::Article')->all;
+	my @articles = $c->model('DB::Article')->search({ domain => $c->user->domain });
+	#my @articles = $c->model('DB::Article')->all;
 	
 	# trim 'content' and convert to HTML (stored as Markdown)
 	my @data;
