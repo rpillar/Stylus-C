19a208515f56c61c42673f085b395171b646a832
rpillar <home@s-imac.local>
2015-03-26 22:05:52 +0000

    updates - to the Register process, the schema changes are required before this will work.

--- Stylus/lib/Stylus/Controller/Register.pm
+++ Stylus/lib/Stylus/Controller/Register.pm
@@ -2,6 +2,8 @@ package Stylus::Controller::Register;
 use Moose;
 use namespace::autoclean;
 
+use Crypt::PBKDF2;
+
 BEGIN { extends 'Catalyst::Controller'; }
 
 __PACKAGE__->config(namespace => 'stylus/register');
@@ -41,16 +43,99 @@ register a 'new' user
 
 =cut
 
-sub create :Path( '/stylus/register/create' ) :Args(0) {
+sub process :Local {
     my ( $self, $c ) = @_;
 
-    # set initial content for 'landing' page
-    $c->stash->{current_view} = 'TT';
-    $c->stash->{template}  = 'index.tt';
-    $c->stash->{initial}   = 'createpartial.tt';
-    $c->stash->{righthalf} = 'createpartialsright.tt';
-}
+    $c->log->debug('Register - register process ....');
 
+    my $name     = $c->request->params->{name};
+    my $email    = $c->request->params->{email};
+    my $username = $c->request->params->{username};
+    my $password = $c->request->params->{password};
+    my $domain   = $c->request->params->{domain};
+
+    # encrypt the password
+    my $pbkdf2 = Crypt::PBKDF2->new(
+        hash_class => 'HMACSHA2',
+        hash_args => {
+            sha_size => 512,
+        },
+        iterations => 10000,
+        salt_len => 10,
+    );
+    my $password_hash = $pbkdf2->generate($password);
+
+    $c->stash->{current_view} = 'JSON_Service';
+
+    # add user to the 'users' table
+    my $error = 0;
+    my $user_data;
+    try {
+        $user_data = $c->model('DB::Users')->create({
+            name          => $name,
+            email_address => $email,
+            username      => $username,
+            password      => $password_hash,
+        });
+    }
+    catch {
+        $c->log->debug('Register - create of new user data failed : ' . $_);
+        $error = 1;
+    };
+    if ( $error ) {
+        $c->stash->{json} = {
+            success => 0,
+            message => 'Register - create of new user data failed'
+        };
+
+        return;
+    }
+
+    # add the domain to the 'domain' table and to the 'user_domains' table
+    my $domain_data;
+    try {
+        $domain_data = $c->model('DB::Domain')->create({
+            domain => $domain
+        });
+    }
+    catch {
+        $c->log->debug('Register - create of new domain data failed : ' . $_);
+        $error = 1;
+    };
+    if ( $error ) {
+        $c->stash->{json} = {
+            success => 0,
+            message => 'Register - create of new domain data failed'
+        };
+
+        return;
+    }
+
+    my $user_domain_data;
+    try {
+        $user_domain_data = $c->model('DB::UserDomain')->create({
+            uid       => $user_data->id,
+            domain_id => $domain_data->id
+        });
+    }
+    catch {
+        $c->log->debug('Register - create of new user_domain data failed : ' . $_);
+        $error = 1;
+    };
+    if ( $error ) {
+        $c->stash->{json} = {
+            success => 0,
+            message => 'Register - create of new user_domain data failed'
+        };
+
+        return;
+    }
+
+    $c->stash->{json} = {
+        success => 1,
+    };
+    return;
+}
 
 =head2 end
 
