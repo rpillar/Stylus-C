f9e1646baf2b374e4b884bee7643165af1a90b8d
Richard Pillar <richardpillar@googlemail.com>
2015-04-17 17:38:37 +0100

    updates - more updates for settings / initial process controller and template.

---
+++ Stylus/lib/Stylus/Controller/Process.pm
@@ -0,0 +1,83 @@
+package Stylus::Controller::Process;
+use Moose;
+use namespace::autoclean;
+
+use DDP;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Stylus::Controller::Content - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=head2 auto
+
+=cut
+
+sub auto :Private {
+    my ( $self, $c ) = @_;
+
+    unless ( $c->user_exists && $c->session->{user_domain_id} ) {
+        $c->log->debug( " User does not exist - redirect to login page ...");
+        if ( $c->req->method eq 'POST' ) {
+            $c->log->debug('Content : session timed out on ajax request');
+        	$c->stash->{current_view} = 'JSON_Service';
+        	$c->stash->{logged_in}    = 0;
+        	$c->detach;
+            return;
+        }
+        else {
+            $c->log->debug('Content : get request ...');
+            $c->response->redirect($c->uri_for('/stylus/login'));
+            $c->detach;
+            return;
+        }
+    }
+
+    return 1;
+}
+
+=head2 index
+
+=cut
+
+sub index :Path( '/stylus/process' ) :Args(0) {
+    my ( $self, $c ) = @_;
+
+    # set initial content for 'landing' page
+    $c->stash->{current_view} = 'TT';
+	$c->stash->{template}  = 'index.tt';
+	$c->stash->{initial}   = 'process.tt';
+	$c->stash->{righthalf} = 'processright.tt';
+}
+
+=head2 end
+
+Attempt to render a view, if needed.
+
+=cut
+
+sub end : ActionClass('RenderView') {}
+
+=head1 AUTHOR
+
+Richard Pillar,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
